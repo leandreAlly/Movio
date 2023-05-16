1cbcb48599a108cc948466fde4576f072b3aaa76
allyleandre <tuyambazeleandre@gmail.com>
2023-05-16 19:42:28 +0200

    wrap my endpoint in try and catch block

--- src/controlllers/genre.controller.js
+++ src/controlllers/genre.controller.js
@@ -2,12 +2,12 @@ import { Genre } from "../model/genre";
 import { validateGenre } from "../validations/genre.validation";
 
 const getAllGenre = async (req, res) => {
-  const genres = await Genre.find().sort("name");
-
-  if (!genres)
-    return res.status(401).json({ message: "Genre collection is empty" });
-
-  return res.status(200).json({ genres });
+  try {
+    const genres = await Genre.find().sort("name");
+    return res.status(200).json({ genres });
+  } catch (err) {
+    return res.status(500).json({ error: err, message: "Server error" });
+  }
 };
 
 export { getAllGenre };
