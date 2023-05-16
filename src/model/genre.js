6466bd82d9b697484f5156846464c11537b5255d
allyleandre <tuyambazeleandre@gmail.com>
2023-05-16 19:11:16 +0200

    create the model of genre

---
+++ src/model/genre.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
+
+export { Genre };
